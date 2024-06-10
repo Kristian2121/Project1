0145830c235b2ccee9668c84dc352b639a6c0f88
Чилингарян Кристиан <kristyanchilyngarian@gmail.com>
2024-06-10 21:02:33 +0300

    first commit

--- TestNow/Program.cs
+++ TestNow/Program.cs
@@ -11,9 +11,13 @@ using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Protocols;
 using Telegram.Bot.Exceptions;
-using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
-namespace Program
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TestNow
 {
     class Program
     {
@@ -21,12 +25,12 @@ namespace Program
 
         static async Task Main(string[] args)
         {
-            botClient = new TelegramBotClient("7467417422:AAEAAMYecsSPSBnNHQw_dxUkrAOm1Xux7oc");
+            botClient = new TelegramBotClient("YOUR_BOT_API_KEY_HERE");
             var me = await botClient.GetMeAsync();
             Console.WriteLine($"Hi, my username is {me.Id} my name is {me.FirstName}.");
             botClient.OnMessage += Bot_OnMessage;
             botClient.StartReceiving();
-            Console.WriteLine("Press any key to exist");
+            Console.WriteLine("Press any key to exit");
             Console.ReadKey();
             botClient.StopReceiving();
         }
@@ -36,37 +40,53 @@ namespace Program
             if (e.Message.Text != null)
             {
                 Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
-
-                string wetherInfo = GetWeatherInfo(e.Message.Text);
-                await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: wetherInfo);
+                string weatherInfo = await GetWeatherInfo(e.Message.Text);
+                await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: weatherInfo);
             }
-
         }
 
-        public async Task GetWeatherInfo(string message)
+        static async Task<string> GetWeatherInfo(string message)
         {
-            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\krist\\source\\repos\\TestNow\\TestNow\\TestWetherData.mdf;Integrated Security=True";
-            string apiURL = "https://api.openweathermap.org/data/2.5/weather?lat={43.0033}&lon={41.0153}&appid={a3d6d40ea2d819407c5923d19ac5a418}";
+            string apiKey = "YOUR_OPENWEATHERMAP_API_KEY_HERE";
+            string apiURL = $"https://api.openweathermap.org/data/2.5/weather?lat=43.0033&lon=41.0153&appid={apiKey}&units=metric";
+
             using (HttpClient client = new HttpClient())
             {
                 try
                 {
-                    HttpResponseMessage response = await client.GetAsync(apiURL); ;
+                    HttpResponseMessage response = await client.GetAsync(apiURL);
                     response.EnsureSuccessStatusCode();
-                    using (SqlConnection connection = new SqlConnection(connectionString))
-                    {
-                        connection.Open();
-                    }
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var weatherData = JsonConvert.DeserializeObject<WeatherResponse>(responseBody);
+                    string weatherDescription = weatherData.weather[0].description;
+                    string temp = weatherData.main.temp.ToString("0.0");
+
+                    return $"Погода: {weatherDescription}, Температура: {temp}°C";
                 }
                 catch (HttpRequestException e)
                 {
-                    Console.WriteLine("\nExceptionCaught!");
+                    Console.WriteLine("\nException Caught!");
                     Console.WriteLine("Message : {0}", e.Message);
-
+                    return "Не удалось получить данные о погоде.";
                 }
-                return $"Погода в {}:";
             }
         }
 
+        public class Weather
+        {
+            public string description { get; set; }
+        }
+
+        public class Main1
+        {
+            public float temp { get; set; }
+        }
+
+        public class WeatherResponse
+        {
+            public Weather[] weather { get; set; }
+            public Main1 main { get; set; }
+        }
     }
 }
